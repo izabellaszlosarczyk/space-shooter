7dcd7832f188df78aee52c5dac57e09bd8bdc24e
Izabella Szlosarczyk <szlosarc@student.agh.edu.pl>
2016-06-27 22:54:12 +0200

    particles

--- js/EnemyEntity.js
+++ js/EnemyEntity.js
@@ -6,24 +6,24 @@ var EnemyEntity = function Entity(world, scene, objSrc, x, y, z, callback) {
 
     var loader = new THREE.ObjectLoader();
     var _self = this;
-    loader.load(objSrc, function ( object ) {
+    loader.load(objSrc, function (object) {
         _self.obj = object;
-        scene.add( object );
+        scene.add(object);
 
         _self.getObject().position.x = x;
         _self.getObject().position.y = y;
         _self.getObject().position.z = z;
 
-        if(callback) {
+        if (callback) {
             callback();
         }
     });
     this.counter = 0;
-    this.directon = new CANNON.Vec3(Math.random(),1,Math.random());
+    this.directon = new CANNON.Vec3(Math.random(), 1, Math.random());
 
-    var halfExtents = new CANNON.Vec3(1,1,1);
+    var halfExtents = new CANNON.Vec3(1, 1, 1);
     var boxShape = new CANNON.Box(halfExtents);
-    this.boxBody = new CANNON.Body({ mass: 5 });
+    this.boxBody = new CANNON.Body({mass: 5});
     this.boxBody.addShape(boxShape);
     this.boxBody.position.x = x;
     this.boxBody.position.y = y;
@@ -39,10 +39,11 @@ var EnemyEntity = function Entity(world, scene, objSrc, x, y, z, callback) {
     //debug off
 
     //disable rotation
-    this.boxBody.angularDamping=1;
+    this.boxBody.angularDamping = 1;
     world.addBody(this.boxBody);
 
     this.toDestroy = false;
+    this.beingDestroyed = false;
 }
 
 EnemyEntity.prototype.getObject = function getObject() {
@@ -52,7 +53,7 @@ EnemyEntity.prototype.getObject = function getObject() {
 EnemyEntity.prototype.update = function update() {
     //update physics
 
-    if(this.getObject()) {
+    if (this.getObject()) {
         this.getObject().position.copy(this.boxBody.position);
         this.getObject().position.y -= 1; //mesh is a bit too high
         this.getObject().quaternion.copy(this.boxBody.quaternion);
@@ -63,9 +64,9 @@ EnemyEntity.prototype.update = function update() {
         //debug off
     }
 
-    this.counter ++;
+    this.counter++;
 
-    if(this.counter % 50) {
+    if (this.counter % 50) {
         //update velocity
         this.boxBody.velocity.x = this.directon.x;
         this.boxBody.velocity.z = this.directon.z;
@@ -76,7 +77,7 @@ EnemyEntity.prototype.update = function update() {
         this.getObject().lookAt(aimAhead);
     }
 
-    if(this.counter > 300) {
+    if (this.counter > 300) {
         this.counter = 0;
 
         this.boxBody.velocity.x = 0;
@@ -84,13 +85,41 @@ EnemyEntity.prototype.update = function update() {
         this.boxBody.velocity.z = 0;
 
         //change direction
-        var xSign = (Math.random() > 0.5)? 1 : -1;
-        var zSign = (Math.random() > 0.5)? 1 : -1;
-        this.directon = new CANNON.Vec3(xSign*Math.random()*ENEMY_SPEED, 0, zSign*Math.random()*ENEMY_SPEED);
+        var xSign = (Math.random() > 0.5) ? 1 : -1;
+        var zSign = (Math.random() > 0.5) ? 1 : -1;
+        this.directon = new CANNON.Vec3(xSign * Math.random() * ENEMY_SPEED, 0, zSign * Math.random() * ENEMY_SPEED);
     }
 }
 
 EnemyEntity.prototype.destroyEntity = function destroyEntity() {
-    this.toDestroy = true;
-    scene.remove(this.obj);
+    if(this.beingDestroyed) {
+        return;
+    }
+    //play explosion animation and destroy
+    this.particleSystem = new THREE.GPUParticleSystem({
+        maxParticles: 250000
+    });
+    for(var i = 0; i < 1000; i++) {
+        this.particleSystem.spawnParticle({
+            position: this.getObject().position.clone(),
+            positionRandomness: .8,
+            velocity: new THREE.Vector3(this.directon.x*ENEMY_SPEED*100, this.directon.y*ENEMY_SPEED*100, this.directon.z*ENEMY_SPEED*100),
+            velocityRandomness: .8,
+            color: 0xaa88ff,
+            colorRandomness: .2,
+            turbulence: .5,
+            lifetime: 2,
+            size: 5,
+            sizeRandomness: 1
+        });
+    }
+    this.scene.add(this.particleSystem);
+    this.scene.remove(this.obj);
+    this.beingDestroyed = true
+
+    var _self = this;
+    setTimeout(function () {
+        _self.toDestroy = true;
+        _self.scene.remove(_self.particleSystem);
+    }, 2500);
 }
